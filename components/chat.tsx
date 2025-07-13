7bc0798b103786ad171e64d5ec9c9b829b1eaedf
Annika Tanner <annikatanner@yahoo.com>
2025-07-13 15:13:31 -0700

    adding branch

---
+++ components/chat.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+export default function Chat(messages) {
+  return (
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.map(message => (
+        <div key={message.id} className="whitespace-pre-wrap">
+          {message.role === 'user' ? 'User: ' : 'AI: '}
+          {message.parts.map((part, i) => {
+            switch (part.type) {
+              case 'text':
+                return <div key={`${message.id}-${i}`}>{part.text}</div>;
+            }
+          })}
+        </div>
+      ))}
+
+    </div>
+  );
+}

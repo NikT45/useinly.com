caceb5e6c75f32562b85419c70b6a3ab75addac1
N1k0405 <nikolasethantanner@gmail.com>
2025-07-22 17:22:00 -0700

    breathing and nav color change

--- components/TalkCircle.tsx
+++ components/TalkCircle.tsx
@@ -17,13 +17,19 @@ export default function TalkCircle() {
         <motion.div
             className={`${mode === 'text' ? 'w-[512px] h-[128px] mt-10 rounded-2xl bg-gray-100' : 'rounded-full w-[300px] h-[300px]'} ${mode === 'voice' ? 'bg-brand-coral' : mode === 'idle' || mode === 'loading' ? 'bg-brand-softPink' : ''} flex items-center justify-between`}
             animate={{
-                scale: mode === 'voice' ? 1 : mode === 'loading' ? [0.95, 1.05, 0.95] : 0.9,
+                scale: mode === 'voice' ? 1 : mode === 'loading' ? [0.95, 1.05, 0.95] : mode === 'idle' ? [0.9, 0.95, 0.9] : 0.9,
             }}
             transition={{
                 ...(mode === 'loading'
                     ? {
                         repeat: Infinity,
                         duration: 1.5,
+                        ease: "easeInOut"
+                    }
+                    : mode === 'idle'
+                    ? {
+                        repeat: Infinity,
+                        duration: 3,
                         ease: "easeInOut"
                     }
                     : {

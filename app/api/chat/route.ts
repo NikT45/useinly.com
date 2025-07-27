ed1693418896cb9f02959c6c930c08d45d0c2bd6
N1k0405 <nikolasethantanner@gmail.com>
2025-07-26 17:09:08 -0700

    changed to google for chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { openai } from '@ai-sdk/openai';
 import { streamText } from 'ai';
+import { google } from "@ai-sdk/google"
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -8,9 +9,27 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = streamText({
-    model: openai('gpt-4o'),
+    model: google('gemini-2.5-flash'),
     messages,
   });
 
   return result.toDataStreamResponse();
 }
+
+{/*import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: openai('gpt-4-turbo'),
+    system: 'You are a helpful assistant.',
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}*/}
